e08635b31ff9d5c238649367a87c661c9af4689a
Admin <jeureankit06@gmail.com>
2026-08-19 22:30:51 +0530

    Add JMeter CLI helper

--- playwright-tdd-framework/commons/JMeter/JMeterCommons.ts
+++ playwright-tdd-framework/commons/JMeter/JMeterCommons.ts
@@ -1,6 +1,22 @@
-
-
+import { exec } from 'child_process';
 
 export class JMeterCommons {
-    
+
+    //Common method to run JMeter test plan from command line
+    private executeCLICommand(command: string): Promise<string> {
+
+        return new Promise((resolve, reject) => {
+
+            exec(command, (error, stdout, stderr) => {
+                if (error) {
+                    reject(`Error executing command: ${error.message}`);
+                } else if (stderr) {
+                    reject(`Error in command output: ${stderr}`);
+                } else {
+                    resolve(stdout);
+                }
+            });
+        });
+    }
+
 }
